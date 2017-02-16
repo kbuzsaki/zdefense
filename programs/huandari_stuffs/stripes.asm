477a84811c3845f7cc4c46c14724c07e8f18b4e5
Huandari Lopez <hlopez137@gmail.com>
2017-02-15 19:51:15 -0800

    Rudimentary support for multiple sprites and their movement

--- programs/huandari_stuffs/stripes.asm
+++ programs/huandari_stuffs/stripes.asm
@@ -1,6 +1,7 @@
 
 org 32768
 
+
 start:
 
     ; Sprite Data:
@@ -9,62 +10,94 @@ start:
     ; 24 total, round up to 32?
     ; 16-bit address | 8-bit offset | 8-bit data
 
-    LD      E, 0
+
+    ; IDEA: Set stack pointer to pointer at top of sprite location storage and just calc HL addresses and push
+    ;       however, tricky bc functions called i.e. pixel_addr and draw_sprite modify sp as well? Not sure what to do
+
+temp_save:                          ; Temporary storage space. May not be best place to put...but for now.... :)
+    defw    0
+
+
+    LD      (temp_save), sp         ; Save original stack pointer
+    
+    LD      sp, $6024               ; Our stack will live at 6024-6000, enough for 18 2-byte words
+
+    LD      C, 4                    ; Set up xpos and ypos in B and C
+    LD      B, 56
+    CALL    pixel_addr              ; then get the screen address
+    PUSH    HL                      ; and save it onto the stack for future reference
+    LD      A, 56                   ; Draw the sprite in its default position
+    CALL    draw_sprite             ; with this call
+
+    LD      C, 10                   ; Same idea here. New sprite in xpos and ypos C and B
+    LD      B, 32
+    CALL    pixel_addr              ; Get address
+    PUSH    HL                      ; Persist it
+    LD      A, 56                   ; Default sprite position
+    CALL    draw_sprite             ; Draw it on screen
+
+    LD      (sprite_iter_sp), sp    ; Store the top of the stack in special space for later use
+    LD      sp, (temp_save)         ; Restore original stack pointer
+
+    LD      D, 56                   ; The global counter for sprite position
+    LD      E, 0                    ; Counter for when to update, modify condition in interrupt_handler
     CALL    interrupt_setup
-
-    LD      C, 4                ; 0000 0100
-    LD      B, 80               ; 0100 0000
-                                ; pixel address: 010|01000 | 000 00100
-
-                                ; pix addr: 0100 1000|010
-                                ; pix addr: 0100 0111   - switch to lower part of screen, end of cell
-
-    CALL    pixel_addr          ; converts our raw x,y into the pixel address for use by draw_sprite
-    ; LD      A, 8
-    LD      A, 56               ; This holds our initial pick for which sprite bitmap to draw, 56 is regular sprite
-    CALL    draw_sprite
-    LD      D, 56               ; Register D is used in the sprite_move_* functions to represent what reg A represents above^
-    ; CALL    sprite_move_right
-    ; CALL    sprite_move_right
-    ; CALL    sprite_move_right
-    ; CALL    sprite_move_right
-    ; CALL    sprite_move_right
-    ; CALL    sprite_move_right
-    ; CALL    sprite_move_right
-    ; CALL    sprite_move_right
-    ; CALL    sprite_move_right
-
-    ; CALL    sprite_move_left
-    ; CALL    sprite_move_right
-    ; CALL    sprite_move_right
-    ; CALL    sprite_move_right
-    ; CALL    sprite_move_right
-    ; CALL    sprite_move_right
-    ; CALL    sprite_move_left
-    ; CALL    sprite_move_left
-    ; CALL    sprite_move_left
-    ; CALL    sprite_move_left
-
 
 
 primary_data_loop:              ; fall through to this routine, HALT until interrupt occurs and start all over again.
     HALT
     JR      primary_data_loop
 
+
+
+move_all_sprites_right:
+    PUSH    BC
+    ; Need HL and D proper values in registers 
+
+    LD      B, D                    ; Preserve current D counter, it'll change between move_right calls but 
+                                    ; we want to keep it consistent between calls till the end, since they're all synchronized
+
+    LD      IX, (sprite_iter_sp)    ; IX register, yikes. not super fast, but easy
+    LD      L, (IX+0)               ; little-endian storage from the initial PUSHes...
+    LD      H, (IX+1)
+
+    CALL    sprite_move_right
+    LD      (IX+0), L               ; Sprite address may be updated, store it back
+    LD      (IX+1), H
+
+    INC     IX                      ; Increase address by 2, to the next sprite data
+    INC     IX
+    
+    ;--------------------------------
+
+    LD      D, B                    ; Keeping D consistent between move_right calls, note we keep the D as it is after this last call
+
+    LD      L, (IX+0)
+    LD      H, (IX+1)
+
+    CALL    sprite_move_right
+    LD      (IX+0), L
+    LD      (IX+1), H
+
+    POP     BC
+    RET
+
+
+
 ; ============================sprites galore============================================
 
 sprite_move_down:
     
-    CALL    clear_cell          ; Much slower, XOR drawing may solve this
+    ; CALL    clear_cell          ; Much slower, XOR drawing may solve this
 
-    LD      A, L
-    ADD     $20
-    LD      L, A
+    ; LD      A, L
+    ; ADD     $20
+    ; LD      L, A
 
-    LD      A, 56
-    CALL    draw_sprite
+    ; LD      A, 56
+    ; CALL    draw_sprite
 
-    RET
+    ; RET
 
 sprite_move_up:
     ;PUSH    HL
@@ -127,6 +160,7 @@ sprite_move_left_overflow:
 ; - D  - Current sprite position of bitmap
 ; - HL may be changed and D will definitely be changed
 sprite_move_right:
+    PUSH    BC
     PUSH    HL                                  ; TODO: Think we could probably get rid of these
 
     ; Next sprite map to be loaded is in A. This is for the original cell
@@ -134,6 +168,17 @@ sprite_move_right:
 
     ; Current sprite position is held in D, increment it to the next position to the right
     ; and then check if we overflowed. If not, just draw the sprite in the new position
+    ; LD      HL, sprite_loc_storage
+    ; ADD     L
+    ; LD      L, A
+
+    ; LD      B, (HL)
+    ; INC     L
+    ; LD      C, (HL)
+    ; LD      H, B
+    ; LD      L, C
+    ;----------------------
+
     LD      A, D
     ADD     8                                   ; Increment bitmap to the next position, i.e. +8
     CP      120                                 ; Did we overflow, are we past the list of acceptable sprites?
@@ -155,9 +200,11 @@ sprite_move_right:
 
 sprite_move_right_end:
     POP     HL                                  ; NORMAL EXIT
+    POP     BC
     RET
 sprite_move_right_overflow:                     ; OVERFLOW CASE EXIT
     POP     HL                                  ; Here, we permanently change the origin address to cell to the right
+    POP     BC
     INC     L                                   ; Our previous address means nothing to us, we don't care about it anymore.
     LD      D, 56                               ; Reset sprite position to default sprite position (centered, neutral)
     LD      A, D                                ; draw_sprite requires sprite pos in reg A
@@ -217,7 +264,6 @@ draw_sprite:
     LD      (HL), D
     INC     H
     
-
     LD      sp, (saved_sp)
     POP     DE
     POP     HL
@@ -269,7 +315,7 @@ interrupt_handler:
     CP      5
     JP      NZ, i_h_cont
 
-    CALL    sprite_move_left
+    CALL    move_all_sprites_right
     LD      E, 0
 
 i_h_cont:
@@ -331,3 +377,7 @@ happy_dude_bitmap:
 
 saved_sp:
     defw    0
+
+sprite_iter_sp:
+    defw    0
+
