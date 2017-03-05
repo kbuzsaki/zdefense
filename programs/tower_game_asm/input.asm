c181c223235934008e25446133c62e9661dbeb6d
Kyle Buzsaki <kbuzsaki@ucsd.edu>
2017-03-04 16:36:07 -0800

    Prefix input code with input_

--- programs/tower_game_asm/input.asm
+++ programs/tower_game_asm/input.asm
@@ -1,39 +1,50 @@
-is_w_down:
+;; Behavior:
+;;   If the key is pressed
+;;     set a to 1
+;;   Else
+;;     set a to 0
+
+input_is_w_down:
     ld bc, $fbfe
     in b, (c)
 	bit 1, b
-	jp z, set_a
+	jp z, input_set_a
 	ld a, 0
 	ret
-is_r_down:
+
+input_is_r_down:
     ld bc, $fbfe
     in b, (c)
 	bit 3, b
-	jp z, set_a
+	jp z, input_set_a
 	ld a, 0
 	ret
-is_a_down:
+
+input_is_a_down:
     ld bc, $fdfe
     in b, (c)
 	bit 0, b
-	jp z, set_a
+	jp z, input_set_a
 	ld a, 0
 	ret
-is_s_down:
+
+input_is_s_down:
     ld bc, $fdfe
     in b, (c)
 	bit 1, b
-	jp z, set_a
+	jp z, input_set_a
 	ld a, 0
 	ret
-is_d_down:
+
+input_is_d_down:
     ld bc, $fdfe
     in b, (c)
 	bit 2, b
-	jp z, set_a
+	jp z, input_set_a
 	ld a, 0
 	ret
-set_a:
+
+input_set_a:
 	ld a, 1
 	ret
 
